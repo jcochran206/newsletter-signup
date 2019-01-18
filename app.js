e339a933e54400c0ffeb25e43d9778cb4897dca5
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-01-17 22:59:36 -0500

    added post method in app.js for email subscriber

--- app.js
+++ app.js
@@ -18,9 +18,31 @@ app.post('/', function(req, res){
   var first = req.body.fname;
   var last = req.body.lname;
   var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed"
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: 'https://us20.api.mailchimp.com/3.0/lists/207f5a848b',
+    method: "POST",
+    headers: {
+      "Authorizations": "jon1 43b2a741c1418a1b07518999f57d0859-us20"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body){
+
+  });
   console.log(first, last, email);
 });
 
 app.listen(port, () => console.log(`application is running on ${port}!`));
-
-//43b2a741c1418a1b07518999f57d0859-us20
